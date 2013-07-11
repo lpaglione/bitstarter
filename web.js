2a31d527830e8a488e5cdae16a5618f4a71cc952
Laura-ORCID <l.paglione@orcid.org>
2013-07-10 23:17:22 -0400

    fixed missing require fs

--- web.js
+++ web.js
@@ -1,9 +1,10 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-	var buffer = new Buffer(fs.readFileSync(index.html));
+	var buffer = new Buffer(fs.readFileSync('index.html'));
 	var hello = buffer.toString();
 	response.sent(hello);
 //  response.send('Hello World! Once again!');
