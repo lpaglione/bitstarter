f2e3adb4c8a63a6c36a29fa13bc0e489934c1e79
Laura-ORCID <l.paglione@orcid.org>
2013-07-14 20:00:19 -0400

    fixed up all of the files to point to the original titles.

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-	var buffer = new Buffer(fs.readFileSync('index-New.html'));
+	var buffer = new Buffer(fs.readFileSync('index.html'));
 	var hello = buffer.toString();
 	response.send(hello);
 //  response.send('Hello World! Once again!');
